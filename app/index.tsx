b3414e35fc9178231ac1ef6b8699cafcb4287d7a
itsKia2 <omerkarimi29@gmail.com>
2024-08-17 00:14:42 -0500

    initial page created for app

---
+++ app/index.tsx
@@ -0,0 +1,46 @@
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+
+import beachImage from '@/assets/meditation-images/beach.webp';
+import CustomButton from '@/components/CustomButton';
+
+const App = () => {
+    return (
+        <View className="flex-1">
+            <ImageBackground
+                source={beachImage}
+                resizeMode="cover"
+                className="flex-1"
+            >
+                <LinearGradient
+                    className="flex-1"
+                    colors={['rgba(0, 0, 0, 0.4)', 'rgba(0, 0, 0, 0.8)']}
+                >
+                    <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+                        <View>
+                            <Text className="text-center text-white font-bold text-4xl">
+                                Thinkerly !
+                            </Text>
+                            <Text className="text-center text-white text-2xl mt-3">
+                                Improving Productivity Everywhere
+                            </Text>
+                        </View>
+
+                        <View>
+                            <CustomButton
+                                onPress={() => console.log('tap')}
+                                title="Focus?"
+                            />
+                        </View>
+
+                        <StatusBar style="light" />
+                    </SafeAreaView>
+                </LinearGradient>
+            </ImageBackground>
+        </View>
+    );
+};
+
+export default App;

08a33b593647ac61be955ad4e36f83db6720e678
itsKia2 <omerkarimi29@gmail.com>
2024-08-26 00:16:40 -0500

    init history.tsx file

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const TimerHistory = () => {
+    return (
+        <View>
+            <Text>Template !</Text>
+        </View>
+    );
+};
+
+export default TimerHistory;
